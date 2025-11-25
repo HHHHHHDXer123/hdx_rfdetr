19bc9170179d8f81e99050ff9b8b60f01e96772e
HHHHHHDXer123 <160241703+HHHHHHDXer123@users.noreply.github.com>
2025-11-25 19:24:21 +0800

    Git LFS commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# hdx_rfdetr
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ea091ef0cc66148d30c8940a010b32b23b9c3a141043cac7e49f254d4a0414b
+size 12
--- train_rfdert.py
+++ train_rfdert.py
@@ -1,16 +1,3 @@
-from rfdetr import RFDETRMedium
-
-model = RFDETRMedium(pretrain_weights="models/rf-detr-medium.pth")
-
-if __name__ == '__main__':  # ✅ Windows 必须要有这行
-    import torch.multiprocessing
-    torch.multiprocessing.freeze_support()  # ✅ 防止 spawn 错误
-
-    model.train(
-        dataset_dir="data/car_coco",
-        epochs=100,
-        batch_size=4,
-        grad_accum_steps=4,
-        lr=1e-4,
-        output_dir="checkpoints"
-    )
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7187db74fd0d8ae39003b8166e1e28e763716cb7d85f7dcc0694566493c8a7d
+size 462
